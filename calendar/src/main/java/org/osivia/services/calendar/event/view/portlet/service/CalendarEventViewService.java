379218177fd83433b0b735b84f96e157ca9d7eb8
jbarberet <jbarberet@atlantis.home>
2018-06-29 16:00:37 +0200

    Modification: la description est stockée dans dc:description et non dans note:note

--- calendar/src/main/java/org/osivia/services/calendar/event/view/portlet/service/CalendarEventViewService.java
+++ calendar/src/main/java/org/osivia/services/calendar/event/view/portlet/service/CalendarEventViewService.java
@@ -32,7 +32,7 @@ public interface CalendarEventViewService extends CalendarService {
     /** Color Nuxeo document property. */
     String COLOR_PROPERTY = "vevent:color";
     /** Description Nuxeo document property. */
-    String DESCRIPTION_PROPERTY = "note:note";
+    String DESCRIPTION_PROPERTY = "dc:description";
     /** Id parent source */
     String ID_PARENT_SOURCE_PROPERTY = "sync:idParentSource";
     
