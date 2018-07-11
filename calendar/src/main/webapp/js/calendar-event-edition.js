eacf7f66bc9114adc53c2ef413cfa0e2d800225a
ckrommenhoek <cedric.krommenhoek@gmail.com>
2018-07-11 17:48:44 +0200

    Ajout de TinyMCE.

--- calendar/src/main/webapp/js/calendar-event-edition.js
+++ calendar/src/main/webapp/js/calendar-event-edition.js
@@ -1,77 +1,74 @@
 $JQry(function() {
-	$JQry(".calendar.calendar-event-edition textarea[name=description]")
-	.each(function(index, element) {
-		if (typeof tinymce != "undefined")
-		{
-			var $textarea = $JQry(element), 
-			id = $textarea.attr("id");
-	
-				if (tinymce.get(id)) {
-					if (!$textarea.data("loaded")) {
-						tinymce.execCommand("mceRemoveEditor", true, id);
-						tinymce.execCommand("mceAddEditor", true, id);
-					}
-				} else {
-					tinymce.init({
-						selector : ".calendar.calendar-event-edition textarea[name=description]",
-						language : "fr_FR",
-						plugins : "autosave link lists noneditable paste",
-						external_plugins : {
-						"osivia_link" : "/osivia-services-editor-helpers/js/link/plugin.min.js"
-					},
-	
-					branding : false,
-					menubar : false,
-					toolbar : "undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist | osivia_link",
-					statusbar : false,
-	
-					element_format : "html",
-					formats : {
-						alignleft : {
-							selector : "p, ul, ol, li",
-							classes : "text-left"
-						},
-						aligncenter : {
-							selector : "p, ul, ol, li",
-							classes : "text-center"
-						},
-						alignright : {
-							selector : "p, ul, ol, li",
-							classes : "text-right"
-						},
-						alignjustify : {
-							selector : "p, ul, ol, li",
-							classes : "text-justify"
-						},
-						bold : {
-							inline : "strong"
-						},
-						italic : {
-							inline : "em"
-						},
-						underline : {
-							inline : "u"
-						},
-						strikethrough : {
-							inline : "s"
-						}
-					},
+	$JQry(".calendar.calendar-event-edition textarea[name=description]").each(function(index, element) {
+        var $textarea = $JQry(element),
+            id = $textarea.attr("id");
 
-					content_css: ["/osivia-portal-custom-web-assets/css/bootstrap.min.css"],
-					height: 200,
+        if (tinymce.get(id)) {
+            if (!$textarea.data("loaded")) {
+                tinymce.execCommand("mceRemoveEditor", true, id);
+                tinymce.execCommand("mceAddEditor", true, id);
+            }
+        } else {
+            tinymce.init({
+                selector: ".calendar.calendar-event-edition textarea[name=description]",
+                language: "fr_FR",
+                plugins: "autosave link lists noneditable paste",
+                external_plugins: {
+                	"osivia_link": "/osivia-services-editor-helpers/js/link/plugin.min.js"
+                },
 
-					// Prevent relative URL conversion
-					convert_urls : false,
-					// Remove style on paste
-					paste_as_text : true,
+                branding: false,
+                menubar: false,
+                toolbar: "undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist | osivia_link",
+                statusbar: false,
+
+                element_format: "html",
+                formats: {
+                    alignleft: {
+                        selector: "p, ul, ol, li",
+                        classes: "text-left"
+                    },
+                    aligncenter: {
+                        selector: "p, ul, ol, li",
+                        classes: "text-center"
+                    },
+                    alignright: {
+                        selector: "p, ul, ol, li",
+                        classes: "text-right"
+                    },
+                    alignjustify: {
+                        selector: "p, ul, ol, li",
+                        classes: "text-justify"
+                    },
+                    bold: {
+                        inline: "strong"
+                    },
+                    italic: {
+                        inline: "em"
+                    },
+                    underline: {
+                        inline: "u"
+                    },
+                    strikethrough: {
+                        inline: "s"
+                    }
+                },
+
+                content_css: ["/osivia-portal-custom-web-assets/css/bootstrap.min.css"],
+                height: 200,
+
+                // Prevent relative URL conversion
+                convert_urls: false,
+                // Remove style on paste
+                paste_as_text: true,
+
+                browser_spellcheck: true
+            });
+        }
+
+        $textarea.data("loaded", true);
+    });
 	
-					browser_spellcheck : true
-					});
-				}
-	
-				$textarea.data("loaded", true);
-		}
-	});
 
 	$JQry(".calendar.calendar-event-edition").each(function(index, element) {
 		var $element = $JQry(element);
