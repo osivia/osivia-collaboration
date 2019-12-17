1c4d52142fe78cdf79f7aaed6f66ffdadf6320f0
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2019-12-17 14:49:07 +0100

    :bug: Contact bug fixes.

--- contact/src/main/java/org/osivia/services/contact/portlet/controller/ContactController.java
+++ contact/src/main/java/org/osivia/services/contact/portlet/controller/ContactController.java
@@ -1,30 +1,8 @@
 package org.osivia.services.contact.portlet.controller;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.Properties;
-
-import javax.annotation.PostConstruct;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
-import javax.portlet.PortletConfig;
-import javax.portlet.PortletContext;
-import javax.portlet.PortletException;
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
-
+import com.github.cage.GCage;
+import com.sun.mail.smtp.SMTPTransport;
+import fr.toutatice.portail.cms.nuxeo.api.CMSPortlet;
 import org.apache.commons.lang.StringUtils;
 import org.osivia.portal.api.context.PortalControllerContext;
 import org.osivia.portal.api.internationalization.Bundle;
@@ -35,6 +13,7 @@ import org.osivia.portal.api.windows.WindowFactory;
 import org.osivia.services.contact.portlet.model.Form;
 import org.osivia.services.contact.portlet.model.validation.ContactFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -50,113 +29,51 @@ import org.springframework.web.portlet.bind.annotation.ResourceMapping;
 import org.springframework.web.portlet.context.PortletConfigAware;
 import org.springframework.web.portlet.context.PortletContextAware;
 
-import com.github.cage.GCage;
-import com.sun.mail.smtp.SMTPTransport;
-
-import fr.toutatice.portail.cms.nuxeo.api.CMSPortlet;
+import javax.annotation.PostConstruct;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.portlet.*;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
 
 @Controller
 @RequestMapping({"VIEW"})
 @SessionAttributes("form")
 public class ContactController
-extends CMSPortlet
-implements PortletConfigAware, PortletContextAware
-{
+        extends CMSPortlet
+        implements PortletConfigAware, PortletContextAware {
     private static final String DEFAULT_VIEW = "view";
+
+    /**
+     * Application context.
+     */
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     private IBundleFactory bundleFactory;
 
     @Autowired
     private INotificationsService notificationService;
-    
-    /** Contact form validator. */
+
+    /**
+     * Contact form validator.
+     */
     @Autowired
-    private ContactFormValidator formValidator;    
+    private ContactFormValidator formValidator;
 
     private PortletConfig portletConfig;
 
     private PortletContext portletContext;
 
-    @PostConstruct
-    public void postConstruct()
-            throws PortletException
-    {
-        super.init(portletConfig);
-    }
-
-    @RenderMapping
-    public String defaultView(RenderRequest request, RenderResponse response)
-            throws PortletException
-    {
-        return "view";
-    }
-
-    @ModelAttribute("form")
-    public Form getForm(PortletRequest request, PortletResponse response)
-    {
-        PortalWindow window = WindowFactory.getWindow(request);
-
-        Bundle bundle = bundleFactory.getBundle(request.getLocale());
-		GCage gcage = new GCage();
-
-        Form form = new Form();
-        form.setTo(window.getProperty("osivia.contact.mailto"));
-        form.setObject(window.getProperty("osivia.contact.object"));
-        form.setToken(gcage.getTokenGenerator().next());
-        form.setFromLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.fromLabel"), bundle.getString("ENTER_EMAIL")));
-        form.setNomLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.nomLabel"), bundle.getString("NAMES")));
-        form.setObjectLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.objectLabel"), bundle.getString("SUBJECT")));
-        form.setBodyLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.bodyLabel"), bundle.getString("MESSAGE")));        
-        return form;
-    }
-
-	@ActionMapping(name="submit", params="send")
-    public void sendMail(ActionRequest request, ActionResponse response, @ModelAttribute("form") @Validated Form form, BindingResult result, SessionStatus status)
-            throws PortletException, IOException
-    {
-    	PortalControllerContext pcc = new PortalControllerContext(getPortletContext(), request, response);
-
-		Bundle bundle = bundleFactory.getBundle(request.getLocale());
-
-		Properties props = System.getProperties();
-
-		Session mailSession = Session.getInstance(props, null);
-
-		MimeMessage msg = new MimeMessage(mailSession);        
-		try
-        {
-            if (!result.hasErrors())
-            {
-            	status.setComplete();
-                InternetAddress from = new InternetAddress(form.getFrom());
-                msg.setFrom(from);
-                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(form.getTo(), false));
-                msg.setSubject(form.getObject(), "UTF-8");
-                Multipart mp = new MimeMultipart();
-                MimeBodyPart htmlPart = new MimeBodyPart();
-                htmlPart.setContent(bodyFormat(form, bundle), "text/html; charset=UTF-8");
-                mp.addBodyPart(htmlPart);
-                msg.setContent(mp);
-                msg.setSentDate(new Date());
-                InternetAddress[] replyToTab = new InternetAddress[1];
-                replyToTab[0] = from;
-                msg.setReplyTo(replyToTab);
-                SMTPTransport t = (SMTPTransport)mailSession.getTransport();
-                t.connect();
-                t.sendMessage(msg, msg.getAllRecipients());
-                t.close();
-                form.setSent(true);
-            }
-        }
-        catch (MessagingException e)
-        {
-            throw new PortletException(e);
-        }	
-    }
-
-    private static String bodyFormat(Form form, Bundle bundle)
-    {
+    private static String bodyFormat(Form form, Bundle bundle) {
         StringBuilder sb = new StringBuilder();
 
         sb.append(bundle.getString("NEW_MAIL") + form.getNom() + " (" + form.getFrom() + ")");
@@ -172,18 +89,85 @@ implements PortletConfigAware, PortletContextAware
         return sb.toString();
     }
 
+    @PostConstruct
+    public void postConstruct()
+            throws PortletException {
+        super.init(portletConfig);
+    }
+
+    @RenderMapping
+    public String defaultView(RenderRequest request, RenderResponse response)
+            throws PortletException {
+        return "view";
+    }
+
+    @ModelAttribute("form")
+    public Form getForm(PortletRequest request, PortletResponse response) {
+        PortalWindow window = WindowFactory.getWindow(request);
+
+        Bundle bundle = bundleFactory.getBundle(request.getLocale());
+        GCage gcage = new GCage();
+
+        Form form = this.applicationContext.getBean(Form.class);
+        form.setTo(window.getProperty("osivia.contact.mailto"));
+        form.setObject(window.getProperty("osivia.contact.object"));
+        form.setToken(gcage.getTokenGenerator().next());
+        form.setFromLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.fromLabel"), bundle.getString("ENTER_EMAIL")));
+        form.setNomLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.nomLabel"), bundle.getString("NAMES")));
+        form.setObjectLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.objectLabel"), bundle.getString("SUBJECT")));
+        form.setBodyLabel(StringUtils.defaultIfBlank(window.getProperty("osivia.contact.bodyLabel"), bundle.getString("MESSAGE")));
+        return form;
+    }
+
+    @ActionMapping(name = "submit", params = "send")
+    public void sendMail(ActionRequest request, ActionResponse response, @ModelAttribute("form") @Validated Form form, BindingResult result, SessionStatus status)
+            throws PortletException, IOException {
+        PortalControllerContext pcc = new PortalControllerContext(getPortletContext(), request, response);
+
+        Bundle bundle = bundleFactory.getBundle(request.getLocale());
+
+        Properties props = System.getProperties();
+
+        Session mailSession = Session.getInstance(props, null);
+
+        MimeMessage msg = new MimeMessage(mailSession);
+        try {
+            if (!result.hasErrors()) {
+                status.setComplete();
+                InternetAddress from = new InternetAddress(form.getFrom());
+                msg.setFrom(from);
+                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(form.getTo(), false));
+                msg.setSubject(form.getObject(), "UTF-8");
+                Multipart mp = new MimeMultipart();
+                MimeBodyPart htmlPart = new MimeBodyPart();
+                htmlPart.setContent(bodyFormat(form, bundle), "text/html; charset=UTF-8");
+                mp.addBodyPart(htmlPart);
+                msg.setContent(mp);
+                msg.setSentDate(new Date());
+                InternetAddress[] replyToTab = new InternetAddress[1];
+                replyToTab[0] = from;
+                msg.setReplyTo(replyToTab);
+                SMTPTransport t = (SMTPTransport) mailSession.getTransport();
+                t.connect();
+                t.sendMessage(msg, msg.getAllRecipients());
+                t.close();
+                form.setSent(true);
+            }
+        } catch (MessagingException e) {
+            throw new PortletException(e);
+        }
+    }
+
     @Override
-    public void setPortletConfig(PortletConfig portletConfig)
-    {
+    public void setPortletConfig(PortletConfig portletConfig) {
         this.portletConfig = portletConfig;
     }
 
     @Override
-    public void setPortletContext(PortletContext portletContext)
-    {
+    public void setPortletContext(PortletContext portletContext) {
         this.portletContext = portletContext;
     }
-    
+
     /**
      * Contactform init binder.
      *
@@ -193,28 +177,26 @@ implements PortletConfigAware, PortletContextAware
     public void formInitBinder(WebDataBinder binder) {
         binder.addValidators(this.formValidator);
         binder.setDisallowedFields("token", "captchaValidate");
-    }    
-    
+    }
+
     /**
-     * @throws IOException 
-     * 
-     *
+     * @throws IOException
      */
     @ResourceMapping("captcha")
     public void captchaImage(ResourceRequest request, ResourceResponse response, @ModelAttribute("form") Form form) throws IOException {
-		GCage gcage = new GCage();
-		
-		response.setContentType("image/" + gcage.getFormat());
-		response.setProperty("Cache-Control", "no-cache");
-		response.setProperty("Progma", "no-cache");
-		
-		gcage.draw(form.getToken(), response.getPortletOutputStream());
-    }        
-    
-    @ActionMapping(name="submit", params="reload")
+        GCage gcage = new GCage();
+
+        response.setContentType("image/" + gcage.getFormat());
+        response.setProperty("Cache-Control", "no-cache");
+        response.setProperty("Progma", "no-cache");
+
+        gcage.draw(form.getToken(), response.getPortletOutputStream());
+    }
+
+    @ActionMapping(name = "submit", params = "reload")
     public void reloadImage(ActionRequest request, ActionResponse response, @ModelAttribute("form") Form form) {
-		GCage gcage = new GCage();
-		form.setToken(gcage.getTokenGenerator().next());
+        GCage gcage = new GCage();
+        form.setToken(gcage.getTokenGenerator().next());
     }
 }
 
--- contact/src/main/java/org/osivia/services/contact/portlet/model/Form.java
+++ contact/src/main/java/org/osivia/services/contact/portlet/model/Form.java
@@ -1,146 +1,133 @@
 package org.osivia.services.contact.portlet.model;
 
-public class Form
-{
-  private String from;
-  private String fromLabel;
-  private String to;
-  private String object;
-  private String objectLabel;
-  private String body;
-  private String bodyLabel;
-  private String nom;
-  private String nomLabel;
-  private String captcha;
-  private String token;
-  private boolean captchaValidate;  
-  private boolean sent;
-  
-  public String getTo()
-  {
-    return to;
-  }
-  
-  public void setTo(String to)
-  {
-    this.to = to;
-  }
-  
-  public String getObject()
-  {
-    return object;
-  }
-  
-  public void setObject(String object)
-  {
-    this.object = object;
-  }
-  
-  public String getBody()
-  {
-    return body;
-  }
-  
-  public void setBody(String body)
-  {
-    this.body = body;
-  }
-  
-  public String getFrom()
-  {
-    return from;
-  }
-  
-  public void setFrom(String from)
-  {
-    this.from = from;
-  }
-  
-  public boolean isSent()
-  {
-    return sent;
-  }
-  
-  public void setSent(boolean sent)
-  {
-    this.sent = sent;
-  }
+import org.osivia.portal.api.portlet.Refreshable;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-  
-  public String getNom()
-  {
-    return nom;
-  }
-  
-  public void setNom(String nom)
-  {
-    this.nom = nom;
-  }
-  
-  
-  public String getFromLabel()
-  {
-    return fromLabel;
-  }
-  
-  public void setFromLabel(String fromLabel)
-  {
-    this.fromLabel = fromLabel;
-  }
-  
-  public String getObjectLabel()
-  {
-    return objectLabel;
-  }
-  
-  public void setObjectLabel(String objectLabel)
-  {
-    this.objectLabel = objectLabel;
-  }
-  
-  public String getBodyLabel()
-  {
-    return bodyLabel;
-  }
-  
-  public void setBodyLabel(String bodyLabel)
-  {
-    this.bodyLabel = bodyLabel;
-  }
-  
-  public String getNomLabel()
-  {
-    return nomLabel;
-  }
-  
-  public void setNomLabel(String nomLabel)
-  {
-    this.nomLabel = nomLabel;
-  }
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Refreshable
+public class Form {
+    private String from;
+    private String fromLabel;
+    private String to;
+    private String object;
+    private String objectLabel;
+    private String body;
+    private String bodyLabel;
+    private String nom;
+    private String nomLabel;
+    private String captcha;
+    private String token;
+    private boolean captchaValidate;
+    private boolean sent;
 
-	public String getCaptcha() {
-		return captcha;
-	}
-	
-	public void setCaptcha(String captcha) {
-		this.captcha = captcha;
-	}
+    public String getTo() {
+        return to;
+    }
 
-	public String getToken() {
-		return token;
-	}
+    public void setTo(String to) {
+        this.to = to;
+    }
 
-	public void setToken(String token) {
-		this.token = token;
-	}
+    public String getObject() {
+        return object;
+    }
 
-	public boolean isCaptchaValidate() {
-		return captchaValidate;
-	}
+    public void setObject(String object) {
+        this.object = object;
+    }
 
-	public void setCaptchaValidate(boolean captchaValidate) {
-		this.captchaValidate = captchaValidate;
-	}
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+    public String getFromLabel() {
+        return fromLabel;
+    }
+
+    public void setFromLabel(String fromLabel) {
+        this.fromLabel = fromLabel;
+    }
+
+    public String getObjectLabel() {
+        return objectLabel;
+    }
+
+    public void setObjectLabel(String objectLabel) {
+        this.objectLabel = objectLabel;
+    }
+
+    public String getBodyLabel() {
+        return bodyLabel;
+    }
+
+    public void setBodyLabel(String bodyLabel) {
+        this.bodyLabel = bodyLabel;
+    }
+
+    public String getNomLabel() {
+        return nomLabel;
+    }
+
+    public void setNomLabel(String nomLabel) {
+        this.nomLabel = nomLabel;
+    }
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isCaptchaValidate() {
+        return captchaValidate;
+    }
+
+    public void setCaptchaValidate(boolean captchaValidate) {
+        this.captchaValidate = captchaValidate;
+    }
 
 }
 
