50e3c0e3710b2ee23ca43443e3339be05c15f88a
ckrommenhoek <cedric.krommenhoek@gmail.com>
2020-04-20 11:25:57 +0200

    :building_construction: Set explicit proxy properties.

--- rss/src/main/java/org/osivia/services/rss/common/utility/RssUtility.java
+++ rss/src/main/java/org/osivia/services/rss/common/utility/RssUtility.java
@@ -2,8 +2,7 @@ package org.osivia.services.rss.common.utility;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -18,6 +17,9 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.XMLEvent;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osivia.services.rss.common.model.FeedRssModel;
@@ -28,15 +30,16 @@ import org.owasp.html.PolicyFactory;
 
 /**
  * Read RSS feed
- * 
- * @author Frédéric Boudan
  *
+ * @author Frédéric Boudan
  */
 public class RssUtility {
 
-	/** logger */
-	protected static final Log logger = LogFactory.getLog(RssUtility.class);	
-	
+    /**
+     * logger
+     */
+    protected static final Log logger = LogFactory.getLog(RssUtility.class);
+
     static final String TITLE = "title";
     static final String DESCRIPTION = "description";
     static final String CHANNEL = "channel";
@@ -49,13 +52,12 @@ public class RssUtility {
     static final String SOURCE = "source";
     static final String ENCLOSURE = "enclosure";
     private static final String MEDIA_CONTENT = "media:content";
-    
+
     /**
      * Read RSS feed.
-     * 
      */
-	public static List<ItemRssModel> readRss(FeedRssModel feed) {
-    	// Restitution d'une map
+    public static List<ItemRssModel> readRss(FeedRssModel feed) {
+        // Restitution d'une map
         String description = "";
         String title = "";
         String link = "";
@@ -68,86 +70,95 @@ public class RssUtility {
         String docid = "";
         Picture visual = null;
         String idConteneur = feed.getSyncId();
-    	ItemRssModel rss = null;
+        ItemRssModel rss = null;
         List<ItemRssModel> list = new ArrayList<ItemRssModel>();
-	    SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM yyyy", Locale.ENGLISH);
-    	
-		try {
-			
+        SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM yyyy", Locale.ENGLISH);
+
+        try {
+
             // Création d'un XMLInputFactory
             XMLInputFactory inputFactory = XMLInputFactory.newInstance();
 
             URL url = null;
-			try {
-				url = new URL(feed.getUrl());
-			} catch (MalformedURLException e) {
-				logger.error("Erreur lors de la lecture du flux :" + e.getMessage());
-			}
-	    	logger.info("Lecture du flux RSS :" +  url);			
-            InputStream in = read(url);
-            if(in != null) {
-            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-            
-            // lecture du document XML 
-            while (eventReader.hasNext()) {
-                XMLEvent event = eventReader.nextEvent();
-                if (event.isStartElement()) {
-                    String localPart = event.asStartElement().getName()
-                            .getLocalPart();
-                    switch (localPart) {
-                    case ITEM:
-                        event = eventReader.nextEvent();
-                        break;
-                    case TITLE:
-                        title = getCharacterData(event, eventReader);
-                        break;
-                    case LINK:
-                        link = getCharacterData(event, eventReader);
-                        break;
-                    case DESCRIPTION:
-                        description = getCharacterDataSanitazier(event, eventReader);
-                        break;
-                    case AUTHOR:
-                        author = getCharacterData(event, eventReader);
-                        break;
-                    case CATEGORY:
-                        category = getCharacterData(event, eventReader);
-                        break;
-                    case ENCLOSURE:
-                        enclosure = getEnclosure(event, eventReader);
-                        break;                        
-                    case GUID:
-                        guid = getCharacterData(event, eventReader);
-                        break;
-                    case PUB_DATE:
-                		try {
-                			pubDate = format.parse(getCharacterData(event, eventReader).substring(0, 16));
-                		} catch (ParseException e) {
-                			e.printStackTrace();
-                		}                    	
-                        break;
-                    case SOURCE:
-                    	sourceRss = getCharacterData(event, eventReader);
-                        break;
-                    }
-                } else if (event.isEndElement()) {
-                    if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
-                    	// Création du document nuxeo contenant le flux RSS
-                        rss = new ItemRssModel(title, link, description, author, pubDate, guid, idConteneur, category, enclosure, sourceRss, visual, docid);   
-                        list.add(rss);
-                        event = eventReader.nextEvent();
-                        continue;
+            try {
+                url = new URL(feed.getUrl());
+            } catch (MalformedURLException e) {
+                logger.error("Erreur lors de la lecture du flux :" + e.getMessage());
+            }
+            logger.info("Lecture du flux RSS :" + url);
+            InputStream inputStream = null;
+            try {
+                Proxy proxy = getProxy();
+
+                URLConnection urlConnection = url.openConnection(proxy);
+                inputStream = urlConnection.getInputStream();
+
+                if (inputStream != null) {
+                    XMLEventReader eventReader = inputFactory.createXMLEventReader(inputStream);
+
+                    // lecture du document XML
+                    while (eventReader.hasNext()) {
+                        XMLEvent event = eventReader.nextEvent();
+                        if (event.isStartElement()) {
+                            String localPart = event.asStartElement().getName()
+                                    .getLocalPart();
+                            switch (localPart) {
+                                case ITEM:
+                                    event = eventReader.nextEvent();
+                                    break;
+                                case TITLE:
+                                    title = getCharacterData(event, eventReader);
+                                    break;
+                                case LINK:
+                                    link = getCharacterData(event, eventReader);
+                                    break;
+                                case DESCRIPTION:
+                                    description = getCharacterDataSanitazier(event, eventReader);
+                                    break;
+                                case AUTHOR:
+                                    author = getCharacterData(event, eventReader);
+                                    break;
+                                case CATEGORY:
+                                    category = getCharacterData(event, eventReader);
+                                    break;
+                                case ENCLOSURE:
+                                    enclosure = getEnclosure(event, eventReader);
+                                    break;
+                                case GUID:
+                                    guid = getCharacterData(event, eventReader);
+                                    break;
+                                case PUB_DATE:
+                                    try {
+                                        pubDate = format.parse(getCharacterData(event, eventReader).substring(0, 16));
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                case SOURCE:
+                                    sourceRss = getCharacterData(event, eventReader);
+                                    break;
+                            }
+                        } else if (event.isEndElement()) {
+                            if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
+                                // Création du document nuxeo contenant le flux RSS
+                                rss = new ItemRssModel(title, link, description, author, pubDate, guid, idConteneur, category, enclosure, sourceRss, visual, docid);
+                                list.add(rss);
+                                event = eventReader.nextEvent();
+                                continue;
+                            }
+                        }
                     }
                 }
+            } finally {
+                IOUtils.closeQuietly(inputStream);
             }
-		}
-        } catch (XMLStreamException e) {
-        	logger.error("Erreur lors de la lecture du flux:" + e.getMessage());
+        } catch (XMLStreamException | IOException e) {
+            logger.error("Erreur lors de la lecture du flux:" + e.getMessage());
         }
-		return list;
+        return list;
     }
 
-	public static String getCharacterData(XMLEvent event, XMLEventReader eventReader)
+    public static String getCharacterData(XMLEvent event, XMLEventReader eventReader)
             throws XMLStreamException {
         String result = "";
         event = eventReader.nextEvent();
@@ -156,41 +167,53 @@ public class RssUtility {
         }
         return result;
     }
-	
-	public static String getCharacterDataSanitazier(XMLEvent event, XMLEventReader eventReader)
+
+    public static String getCharacterDataSanitazier(XMLEvent event, XMLEventReader eventReader)
             throws XMLStreamException {
         String result = "";
         event = eventReader.nextEvent();
         if (event instanceof Characters) {
-			// Add sanitizer
-			PolicyFactory policy = new HtmlPolicyBuilder().allowElements("b", "i", "u", "em", "strong").toFactory();
-			result = policy.sanitize(event.asCharacters().getData());
+            // Add sanitizer
+            PolicyFactory policy = new HtmlPolicyBuilder().allowElements("b", "i", "u", "em", "strong").toFactory();
+            result = policy.sanitize(event.asCharacters().getData());
         }
         return result;
-    }	
-	
-	public static String getEnclosure(XMLEvent event, XMLEventReader eventReader)
+    }
+
+    public static String getEnclosure(XMLEvent event, XMLEventReader eventReader)
             throws XMLStreamException {
         String result = "";
         QName hrefQName = new QName("", "url");
         result = event.asStartElement().getAttributeByName(hrefQName).getValue();
 
         return result;
-    }    
-	
-	public static InputStream read(URL url) {
-		try {
-			return url.openStream();
-		} catch (IOException e) {
-        	logger.error("Erreur lors de la lecture du flux:" + e.getMessage());
-    		return null;
-		}
-	}
-    	
-	// Algo à mettre en place
-	// --> 1 : Lecture d'une url ex: le monde
-	// --> 2 : rapatriement du flux
-	// --> 3 : parsing du flux
-	// --> 4 : enregistrement des données dans une map
-	// --> 5 : Que fait on des anciens Items du flux ? Lecture des anciens Items et si les nouveaux Items sont supérieurs à 5 (nb de flux qui peuvent apparaître) on peut suprimer les anciens flux
+    }
+
+    /**
+     * Get proxy.
+     *
+     * @return proxy
+     */
+    private static Proxy getProxy() {
+        String proxyHost = System.getProperty("http.proxyHost");
+        int proxyPort = NumberUtils.toInt(System.getProperty("http.proxyPort"));
+
+        Proxy proxy;
+        if (StringUtils.isEmpty(proxyHost)) {
+            proxy = Proxy.NO_PROXY;
+        } else {
+            SocketAddress socketAddress = new InetSocketAddress(proxyHost, proxyPort);
+            proxy = new Proxy(Proxy.Type.HTTP, socketAddress);
+        }
+        
+        return proxy;
+    }
+
+
+    // Algo à mettre en place
+    // --> 1 : Lecture d'une url ex: le monde
+    // --> 2 : rapatriement du flux
+    // --> 3 : parsing du flux
+    // --> 4 : enregistrement des données dans une map
+    // --> 5 : Que fait on des anciens Items du flux ? Lecture des anciens Items et si les nouveaux Items sont supérieurs à 5 (nb de flux qui peuvent apparaître) on peut suprimer les anciens flux
 }
